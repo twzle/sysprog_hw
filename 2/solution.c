e373ff336755f0aa4217612f04b2beb973d7f2c6
Alexey Efremov <tw1zle8@gmail.com>
2024-02-29 00:12:17 +0300

    valid pipes

--- 2/solution.c
+++ 2/solution.c
@@ -11,8 +11,29 @@
 #include <fcntl.h>
 #include <stdlib.h>
 
+void initialize_pipes(int *pipes_count, int **fd)
+{
+	for (int i = 0; i < *pipes_count; i++)
+	{
+		fd[i] = (int *)&fd[*pipes_count] + i * 2;
+		pipe(fd[i]);
+	}
+}
 
-int handle_exit_cmd(char *cmd, int* exit_status){
+void count_pipes(const struct expr *e, int *pipes_count)
+{
+	while (e != NULL)
+	{
+		if (e->type == EXPR_TYPE_PIPE)
+		{
+			*pipes_count += 1;
+		}
+		e = e->next;
+	}
+}
+
+int handle_exit_cmd(char *cmd, int *exit_status)
+{
 	if (strcmp(cmd, "exit") == 0)
 	{
 		*exit_status = 1;
@@ -22,234 +43,122 @@ int handle_exit_cmd(char *cmd, int* exit_status){
 	return 0;
 }
 
-void execute_command(const struct expr *e, const struct command_line *line, int *exit_status, int *before_pipe, int *after_pipe, int* fd_prev, int* fd_next, int* p2p_count)
+void free_cmd_atributes(int** fd, int* child_pid){
+	free(fd);
+	free(child_pid);
+}
+
+int execute_command(const struct expr *e, const struct command_line *line, int **fd, int *child_pid, int pipes_count, int cmd_id)
 {
 	char *argv[e->cmd.arg_count + 2];
 	argv[0] = e->cmd.exe;
-	for (uint32_t i = 0; i < e->cmd.arg_count; i++)
+	for (uint32_t i = 0; i < e->cmd.arg_count; ++i)
 	{
 		argv[i + 1] = e->cmd.args[i];
 	}
 	argv[e->cmd.arg_count + 2 - 1] = NULL;
 
-	*exit_status = 0;
-	int status = 0;
+	int exit_flag = 0;
+
 	if (strcmp(e->cmd.exe, "cd") == 0)
 	{
-		if (e->cmd.arg_count == 1)
-		{
+		if (pipes_count == 0)
+		{	
 			int error = chdir(argv[1]);
 			if (error != 0)
 				printf("bash: %s: out: No such file or directory\n", e->cmd.exe);
 		}
-		else if (e->cmd.arg_count > 1)
-			printf("bash: %s: too many arguments\n", e->cmd.exe);
+
+		return 0;
 	}
-	else
+
+	if (strcmp(e->cmd.exe, "exit") == 0)
 	{
-		if (*after_pipe == 0 && *before_pipe == 0)
-		{
-			if (handle_exit_cmd(e->cmd.exe, exit_status) != 0){ return; }
-			int pid = fork();
-			if (pid == 0)
-			{
-				if (line->out_type == OUTPUT_TYPE_STDOUT)
-				{	
-					int error = execvp(e->cmd.exe, argv);
-					if (error != 0)
-					{
-						execlp("sh", "sh", "-c", "exit", NULL);
-					}
-				}
-				else if (line->out_type == OUTPUT_TYPE_FILE_NEW)
-				{
-					int file = open(line->out_file, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-					dup2(file, STDOUT_FILENO);
-					execvp(e->cmd.exe, argv);
-				}
-				else if (line->out_type == OUTPUT_TYPE_FILE_APPEND)
-				{
-					int file = open(line->out_file, O_RDWR | O_APPEND, S_IRUSR | S_IWUSR);
-					dup2(file, STDOUT_FILENO);
-					execvp(e->cmd.exe, argv);
-				}
-			}
-			else if (pid < 0)
-			{
-			}
-			else if (pid > 0)
-			{
-				if (wait(&status) >= 0)
-				{
-					// printf("Child process exited with %d status\n", WEXITSTATUS(status));
-				}
-			}
-		} else if (*after_pipe == 1 && *before_pipe == 0) {
-			int pid = fork();
-			if (pid == 0)
-			{
-				dup2(fd_prev[1], STDOUT_FILENO);
-				close(fd_prev[1]);
-				close(fd_prev[0]);
-				close(fd_next[1]);
-				close(fd_next[0]);
-				int error = execvp(e->cmd.exe, argv);
-				if (error != 0)
-				{
-					execlp("sh", "sh", "-c", "exit", NULL);
-				}
-			}
-			else if (pid < 0)
-			{
-			}
-			else if (pid > 0)
-			{	 
-				close(fd_prev[1]);
+		if (pipes_count == 0)
+		{	
+			return 1;
+		}
 
-				if (wait(&status) >= 0)
-				{
-					// printf("Child process exited with %d status\n", WEXITSTATUS(status));
-				}
-			}
-
-		} else if (*after_pipe == 1 && *before_pipe == 1) {
-
-			int pid = fork();
-			if (pid == 0)
-			{
-				if (*p2p_count % 2 == 0){
-					dup2(fd_prev[0], STDIN_FILENO);
-					dup2(fd_next[1], STDOUT_FILENO);
-					close(fd_prev[1]);
-					close(fd_prev[0]);
-					close(fd_next[1]);
-					close(fd_next[0]);
-				} else {
-					dup2(fd_next[0], STDIN_FILENO);
-					dup2(fd_prev[1], STDOUT_FILENO);
-					close(fd_prev[1]);
-					close(fd_prev[0]);
-					close(fd_next[1]);
-					close(fd_next[0]);
-				}
-				int error = execvp(e->cmd.exe, argv);
-				if (error != 0)
-				{
-					execlp("sh", "sh", "-c", "exit", NULL);
-				}
-			}
-			else if (pid < 0)
-			{
-			}
-			else if (pid > 0)
-			{	 
-				if (*p2p_count % 2 == 0){
-					close(fd_next[1]);
-					close(fd_prev[0]);
-					pipe(fd_prev);
-				} else {
-					close(fd_prev[1]);
-					close(fd_next[0]);
-					pipe(fd_next);
-				}
-
-				if (wait(&status) >= 0)
-				{
-					*p2p_count += 1;
-					// printf("Child process exited with %d status\n", WEXITSTATUS(status));
-				}
-			}	
-		} else if (*after_pipe == 0 && *before_pipe == 1) {
-			int pid = fork();
-			if (pid == 0)
-			{
-				if (line->out_type == OUTPUT_TYPE_STDOUT)
-				{	
-					if (*p2p_count % 2 == 1){
-						dup2(fd_next[0], STDIN_FILENO);
-						close(fd_prev[0]);
-						close(fd_prev[1]);
-						close(fd_next[0]);
-						close(fd_next[1]);
-					} else {
-						dup2(fd_prev[0], STDIN_FILENO);
-						close(fd_prev[0]);
-						close(fd_prev[1]);
-						close(fd_next[0]);
-						close(fd_next[1]);
-					}
-					int error = execvp(e->cmd.exe, argv);
-					if (error != 0)
-					{
-						execlp("sh", "sh", "-c", "exit", NULL);
-					}
-				}
-				else if (line->out_type == OUTPUT_TYPE_FILE_NEW)
-				{
-					if (*p2p_count % 2 == 1){
-						dup2(fd_next[0], STDIN_FILENO);
-						close(fd_prev[0]);
-						close(fd_prev[1]);
-						close(fd_next[0]);
-						close(fd_next[1]);
-					} else {
-						dup2(fd_prev[0], STDIN_FILENO);
-						close(fd_prev[0]);
-						close(fd_prev[1]);
-						close(fd_next[0]);
-						close(fd_next[1]);
-					}
-					
-					int file = open(line->out_file, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-					dup2(file, STDOUT_FILENO);
-					close(file);
-					execvp(e->cmd.exe, argv);
-				}
-				else if (line->out_type == OUTPUT_TYPE_FILE_APPEND)
-				{
-					if (*p2p_count % 2 == 1){
-						dup2(fd_next[0], STDIN_FILENO);
-						close(fd_prev[0]);
-						close(fd_prev[1]);
-						close(fd_next[0]);
-						close(fd_next[1]);
-					} else {
-						dup2(fd_prev[0], STDIN_FILENO);
-						close(fd_prev[0]);
-						close(fd_prev[1]);
-						close(fd_next[0]);
-						close(fd_next[1]);
-					}
-
-					int file = open(line->out_file, O_RDWR | O_APPEND, S_IRUSR | S_IWUSR);
-					dup2(file, STDOUT_FILENO);
-					close(file);
-					execvp(e->cmd.exe, argv);
-				}
-			}
-			else if (pid < 0)
-			{
-			}
-			else if (pid > 0)
-			{	
-				close(fd_prev[0]);
-				close(fd_prev[1]);
-				close(fd_next[0]);
-				close(fd_next[1]);
-
-				if (wait(&status) >= 0)
-				{
-					// printf("Child process exited with %d status\n", WEXITSTATUS(status));
-				}
-			}
-		}	
+		exit_flag = 1;
 	}
 
-	return;
+	if (strcmp(e->cmd.exe, "#") == 0){
+		return 2;
+	}
+
+	int pid = fork();
+	if (pid == 0)
+	{
+		if (cmd_id > 0)
+		{
+			int new_fd = dup2(fd[cmd_id - 1][0], STDIN_FILENO);
+			if (new_fd == -1)
+			{
+				perror("dup2");
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		if (cmd_id < pipes_count)
+		{	
+			int new_fd = dup2(fd[cmd_id][1], STDOUT_FILENO);
+			if (new_fd == -1)
+			{
+				perror("dup2");
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		if (cmd_id == pipes_count) {
+			int new_fd;
+			if (line->out_type == OUTPUT_TYPE_FILE_NEW){
+				int file = open(line->out_file, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+				new_fd = dup2(file, STDOUT_FILENO);
+				close(file);
+			} else if (line->out_type == OUTPUT_TYPE_FILE_APPEND){
+				int file = open(line->out_file, O_RDWR | O_APPEND, S_IRUSR | S_IWUSR);
+				new_fd = dup2(file, STDOUT_FILENO);
+				close(file);
+			}
+
+			if (new_fd == -1)
+			{
+				perror("dup2");
+				exit(EXIT_FAILURE);
+			}
+
+		}
+
+		for (int j = 0; j < pipes_count; j++)
+		{
+			close(fd[j][0]);
+			close(fd[j][1]);
+		}
+
+		if (exit_flag == 1){
+			execlp("sh", "sh", "-c", "exit", NULL);
+		}
+
+		int error = execvp(e->cmd.exe, argv);
+		if (error != 0)
+		{
+			// fprintf(stderr, "exec %d", error);
+			exit(error);
+		}
+	}
+	else if (pid > 0)
+	{
+		child_pid[cmd_id] = pid;
+	}
+	else if (pid == -1)
+	{
+	}
+
+	return 0;
 }
 
 static void
-execute_command_line(const struct command_line *line, int *exit_status)
+execute_command_line(const struct command_line *line, int* exit_status)
 {
 	/* REPLACE THIS CODE WITH ACTUAL COMMAND EXECUTION */
 
@@ -257,10 +166,11 @@ execute_command_line(const struct command_line *line, int *exit_status)
 	// printf("================================\n");
 	// printf("Command line:\n");
 	// printf("Is background: %d\n", (int)line->is_background);
-	// printf("Output: ");
+	// printf("Output: "); 
 	if (line->out_type == OUTPUT_TYPE_STDOUT)
 	{
 		// printf("stdout\n");
+
 	}
 	else if (line->out_type == OUTPUT_TYPE_FILE_NEW)
 	{
@@ -276,12 +186,16 @@ execute_command_line(const struct command_line *line, int *exit_status)
 	}
 	// printf("Expressions:\n");
 	const struct expr *e = line->head;
+	int pipes_count = 0;
+	int cmd_id = 0;
+	*exit_status = 0;
 
-	int before_pipe = 0, after_pipe = 0;
-	int p2p_count = 0;
-	int fd_prev[2], fd_next[2];
-	pipe(fd_prev);
-	pipe(fd_next);
+	count_pipes(line->head, &pipes_count);
+
+	int **fd = malloc(pipes_count * sizeof(int *) + pipes_count * sizeof(int) * 2);
+	int *child_pid = malloc((pipes_count + 1) * sizeof(int));
+	initialize_pipes(&pipes_count, fd);
+
 	while (e != NULL)
 	{
 		if (e->type == EXPR_TYPE_COMMAND)
@@ -291,19 +205,17 @@ execute_command_line(const struct command_line *line, int *exit_status)
 			// for (uint32_t i = 0; i < e->cmd.arg_count; ++i)
 			// 	printf(" %s", e->cmd.args[i]);
 			// printf("\n");
-			if (e->next != NULL && e->next->type == EXPR_TYPE_PIPE)
-			{
-				after_pipe = 1;
+
+			*exit_status = execute_command(e, line, fd, child_pid, pipes_count, cmd_id);
+			if (*exit_status == 1){
+				break;
+			} else if (*exit_status == 2){
+				cmd_id--;
 			}
-			else
-			{
-				after_pipe = 0;
-			}
-			execute_command(e, line, exit_status, &before_pipe, &after_pipe, fd_prev, fd_next, &p2p_count);
+			cmd_id++;
 		}
 		else if (e->type == EXPR_TYPE_PIPE)
 		{
-			before_pipe = 1;
 			// printf("\tPIPE\n");
 		}
 		else if (e->type == EXPR_TYPE_AND)
@@ -320,6 +232,28 @@ execute_command_line(const struct command_line *line, int *exit_status)
 		}
 		e = e->next;
 	}
+
+	for (int i = 0; i < pipes_count; i++)
+	{
+		close(fd[i][0]);
+		close(fd[i][1]);
+	}
+
+	int wstatus;
+	for (int i = 0; i < pipes_count + 1; i++)
+	{
+		waitpid(child_pid[i], &wstatus, 0);
+		if (WIFEXITED(wstatus))
+		{
+			// printf("parent: child_%d exited normally with status %d\n", i, WEXITSTATUS(wstatus));
+		}
+		if (WIFSIGNALED(wstatus))
+		{
+			// printf("parent: child_%d was terminated by signal %d\n", i, WTERMSIG(wstatus));
+		}
+	}
+
+	free_cmd_atributes(fd, child_pid);
 }
 
 int main(void)
